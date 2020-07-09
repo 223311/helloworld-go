dba8b879ed15e8c088d3ddf08805bc2214967eed
Sankar <panda.bhabani31@gmail.com>
2020-07-09 15:49:46 +0530

    Added v10 in controller

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Guest V9")
+	fmt.Fprintf(w, "Hello Guest V10")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
